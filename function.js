66d3fe64d837a1f31fee5a81e64d07c4ed4a87ad
Mmdrza001 <97808174+Mmdrza001@users.noreply.github.com>
2022-01-17 18:05:00 +0330

    h1
    new

---
+++ function.js
@@ -0,0 +1,12 @@
+function tamas(){
+	document.getElementById("footerText").innerHTML="<p style=\"color: purple;padding:1px 60px 1px 1px;\"> شماره تماس : 09176719691  </p><p style=\"color: purple;padding:1px 60px 1px 1px;\"> ایمیل : mmdrzahshyar@gmail.com </p>";
+}
+function darbare(){
+	document.getElementById("footerText").innerHTML="<p style=\"color: purple;padding:1px 60px 1px 1px;\">  این سایت برای ارائه ی پروژه پایان نامه دانشگاهی طراحی شده است</p>";
+}
+function enteqad(){
+	document.getElementById("footerText").innerHTML="<a style=\"padding:1px 60px 1px 1px;\" href=\"mailto: mmdrzahshyar@gmail.com\">برای ارسال انتقادات و پیشنهادات اینجا را کلیک کنید</a>";
+}
+function tabliq(){
+	document.getElementById("footerText").innerHTML="<a style=\"padding:1px 60px 1px 1px;\" href=\"mailto: mmdrzahshyar@gmail.com\">برای درخواست تبلیغات اینجا را کلیک کنید</a>";
+}
